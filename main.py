989319a1b2aef76403e173b929292be25650a165
Rajnish Kumar <raj.kr01001@gmail.com>
2025-03-26 16:46:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from dash import Input, Output
 df= sns.load_dataset('tips')
 
 app = dash.Dash(__name__)
+server= app.server
 
 app.layout = html.Div([
         html.H1('Tips Dataset Analysis'),
@@ -41,4 +42,4 @@ def update_graph(x_axis):
     
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8050)
+    app.run(debug=True, port=8050, host='0.0.0.0')
